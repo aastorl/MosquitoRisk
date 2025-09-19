e23c14585cace16b05492f373360447d4117cb59
aastorl <aastorl79@gmail.com>
2025-09-19 19:20:11 -0300

    ui implementations and fixes

---
+++ MosquitOFF/Views/LaunchScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchScreenView.swift
+//  MosquitOFF
+//
+//  Created by Astor Ludueña  on 15/09/2025.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
---
+++ MosquitOFF/Views/NotificationSettingsSheet.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationSettingsSheet.swift
+//  MosquitOFF
+//
+//  Created by Astor Ludueña  on 19/09/2025.
+//
+
+import SwiftUI
+
+struct NotificationSettingsSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationSettingsSheet()
+}
